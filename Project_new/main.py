f0a589c789f259077ab361d6c9044bc1fe22b217
Silias23 <siafakasil@hotmail.com>
2022-12-08 17:06:09 -0500

    update

--- Project_new/main.py
+++ Project_new/main.py
@@ -1,21 +1,19 @@
 import tracking
 import ocr
 import freq
+import easyocr
 import os
-import ocrmain
-import multiprocessing
 
 def main():
+    #reset the results folder
     ocr.cleandir()
-    track = tracking.plateRecognition(model_name='best.pt')
+    #create the ocr reader object that will be used later
+    reader = easyocr.Reader(['en'])
+    #create an instance of custom plate recognition tracking that uses custom trained yolov5 model to identify the location of the license plate on each frame
+    track = tracking.plateRecognition('best.pt')
     track()
-
-    # objs = [ocrmain.OCRRecognition(path=f'Results\TrackedPlates\Tracked{i}.jpg') for i in range(len(os.listdir('Results\TrackedPlates')))]
-    # for obj in objs:
-    #     obj()
-        # rec = ocrmain.OCRRecognition(path=f'Results\TrackedPlates\Tracked{i}.jpg')
-        # rec()
-    ocr.findLicenses()
+    #run the processed pictures through the ocr reader
+    ocr.findLicenses(reader)
     plate = freq.find_frequency()
     print(plate)
 
